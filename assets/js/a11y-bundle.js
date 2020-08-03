1bdf05af02fa37d454d4a707d23767de4bedec0c
Deyan Ginev <deyan.ginev@gmail.com>
2020-08-03 16:34:14 -0400

    bundle all JavaScript assets together

---
+++ assets/js/a11y-bundle.js
@@ -0,0 +1,21 @@
+---
+---
+{%include_relative jquery-3.5.1.min.js %}
+
+{%include_relative highlight.min.js %}
+
+{%include_relative xml.min.js %}
+
+{%include_relative LaTeXML-maybeMathjax.js %}
+
+{%include_relative sre_browser.js %}
+
+{%include_relative mini-language.js %}
+
+{%include_relative narrate-style-full.js %}
+
+{%include_relative narrate-style-phrase.js %}
+
+{% include_relative mini-narrate.js %}
+
+{% include_relative a11y.js %}
---
+++ assets/js/narrate-style-phrase.js
@@ -0,0 +1,46 @@
+
+function phrase_narrate_switch(op, arg_narrations, fixity) {
+  switch(op) {
+    case 'math':
+    case 'mrow':
+    case 'functional-power':
+    case 'multirelation':
+    case 'index':
+      return arg_narrations.join(" ");
+    case 'msub':
+      return infix("Subscript", arg_narrations);
+    case 'msup':
+      return infix("Superscript", arg_narrations);
+    case 'msubsup':
+      return [arg_narrations[0], "Subscript", arg_narrations[1], "Superscript", arg_narrations[2]].join(" ");
+    case 'divide':
+      return infix('divided by', arg_narrations);
+    case 'binomial':
+      return infix('choose', arg_narrations);
+    case 'element-of':
+      return infix('in', arg_narrations);
+    case 'open-interval':
+    case 'closed-interval':
+    case 'open-closed-interval':
+    case 'closed-open-interval':
+      return infix('to', arg_narrations);
+    case 'integral':
+      return np_of(op,arg_narrations[0]+' d '+arg_narrations[1]);
+    case 'derivative-implicit-variable':
+      return infix(" derivative of ", [numeral(arg_narrations[1]), arg_narrations[0]]);
+    default:
+      switch (fixity) {
+        case 'prefix': return np_of(op, arg_narrations[0]);
+        case 'infix':
+        case 'nary-infix':
+          return infix(op, arg_narrations);
+        case 'nary-implied':
+          return np_of(op, list_and(arg_narrations));
+        case 'postfix': return postfix(op, arg_narrations[0]);
+        case 'subfix': return arg_narrations[0] + " at " + arg_narrations[1];
+        case 'superfix': return arg_narrations[0] + " to the " + arg_narrations[1];
+        case 'fenced': return np_of(op, list_and(arg_narrations));
+        default: return np_of(op, list_and(arg_narrations));
+      }
+  }
+}
